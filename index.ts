845e3c693b7e278baa10b406cfd2e360bf1dd911
unknown <msilva@synergyonline.com>
2022-03-07 14:36:55 -0500

    updating vercel

--- index.ts
+++ index.ts
@@ -21,10 +21,10 @@ app.use('/api/profile', profileRoutes);
 app.use('/api/posts', postsRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, './client/build')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, './client/build/index.html'));
     });
 }
 
