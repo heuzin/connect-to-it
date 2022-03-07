e39575f00ecd71ab16e9cfcd5c87539f0c931de0
unknown <msilva@synergyonline.com>
2022-03-07 13:43:28 -0500

    build commands

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import usersRoutes from './routes/api/users';
 import profileRoutes from './routes/api/profile';
 import postsRoutes from './routes/api/posts';
 import authRoutes from './routes/api/auth';
+import path from 'path';
 
 dotenv.config();
 
@@ -14,12 +15,18 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('API Running'));
-
 app.use('/api/users', usersRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/profile', profileRoutes);
 app.use('/api/posts', postsRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
